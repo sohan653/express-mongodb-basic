d4d25609a95ea891ad182cdc84778d1f8d20cde0
sohanur <sohanurl653@gmail.com>
2022-04-23 22:48:32 +0600

    mongo added

---
+++ _steps.js
@@ -0,0 +1,27 @@
+/* 
+________________
+node server
+________________
+*create folder
+*npm init
+*npm install ,express,cors,mongodb
+*script-dev:nodemon index.js
+*create index.js
+
+___________________________
+create mongodb atlas account
+____________________________
+*sign up with google
+*create a cluster
+*create a db user
+*natwork acess ip adress allow all
+*database >connect> code copy;
+
+_____________
+crud operation
+_______________
+1.node mongodb crud>fundamental
+2.create run funtion
+
+
+ */
